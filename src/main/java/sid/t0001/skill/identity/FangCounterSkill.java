966d26fc3d0cecf9ed5f375e44ac0c2dbaf9ec64
Sidtheplayer <140822160+Sidtheplayer@users.noreply.github.com>
2025-09-29 21:03:33 +0530

    gyatt1

--- src/main/java/sid/t0001/skill/identity/FangCounterSkill.java
+++ src/main/java/sid/t0001/skill/identity/FangCounterSkill.java
@@ -1,15 +1,21 @@
 package sid.t0001.skill.identity;
 
 import com.google.common.collect.Maps;
+import com.lowdragmc.photon.client.fx.*;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Vector3d;
 import sid.t0001.gameasset.t0001Animations;
 import sid.t0001.skill.t0001SkillDataKeys;
 import yesman.epicfight.api.animation.AnimationManager.AnimationAccessor;
@@ -31,12 +37,14 @@ import yesman.epicfight.world.capabilities.item.CapabilityItem.WeaponCategories;
 import yesman.epicfight.world.capabilities.item.WeaponCategory;
 import yesman.epicfight.world.entity.eventlistener.PlayerEventListener;
 import yesman.epicfight.world.entity.eventlistener.PlayerEventListener.EventType;
+import yesman.epicfight.world.entity.eventlistener.TakeDamageEvent;
 
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.BiFunction;
+
 
 public class FangCounterSkill extends Skill {
     private static final UUID EVENT_UUID = UUID.fromString("7eaf7af7-2622-40a8-acbc-dee925e3aec3");
@@ -49,6 +57,57 @@ public class FangCounterSkill extends Skill {
                 .setCategory(SkillCategories.IDENTITY)
                 .setActivateType(ActivateType.ONE_SHOT)
                 .setResource(Resource.NONE);
+    }
+
+    private static void accept(TakeDamageEvent.Attack event) {
+        if (event.getResult() != AttackResult.ResultType.BLOCKED) return;
+
+        ServerPlayer serverPlayer = event.getPlayerPatch().getOriginal();
+        if (serverPlayer == null) return;
+//Graheh
+
+        String command = "";
+        if (!serverPlayer.level().isClientSide() && event.isParried()) {
+
+            BiFunction<Entity, Entity, Vector3d> FRONT_OF_EYES = (target, attacker) -> {
+                Vec3 eyePosition = target.getEyePosition();
+                Vec3 viewVec = target.getLookAngle().scale(1.0D);
+
+                return new Vector3d(eyePosition.x + viewVec.x, eyePosition.y + viewVec.y - 0.27D, eyePosition.z + viewVec.z);
+            };
+
+
+            Vector3d frontOfEyes = FRONT_OF_EYES.apply(serverPlayer, null);
+
+
+            BlockPos effectPos = new BlockPos((int) frontOfEyes.x, (int) frontOfEyes.y, (int) frontOfEyes.z);
+
+            FX breakclashfx = FXHelper.getFX(new ResourceLocation("photon:breakclash4"));
+            BlockEffect parry_effect = new BlockEffect(breakclashfx, serverPlayer.level(), effectPos);
+
+            double offsetX = frontOfEyes.x - effectPos.getX() - 0.5; // we gonna do -0.5 because BlockEffect adds 0.5
+            double offsetY = frontOfEyes.y - effectPos.getY() - 0.5;
+            double offsetZ = frontOfEyes.z - effectPos.getZ() - 0.5;
+
+            parry_effect.setOffset(offsetX, offsetY, offsetZ);
+            parry_effect.setRotation(0, 0, 0);
+            parry_effect.setScale(0.95, 0.95, 0.95);
+            parry_effect.setDelay(0);
+            parry_effect.setForcedDeath(false);
+            parry_effect.setAllowMulti(true);
+
+            parry_effect.start();
+
+        } else {
+            command = "/photon fx photon:block entity @s 0 0.35 0 0 0 0 1 1 1 0 true true xrot";
+        }
+
+        Objects.requireNonNull(serverPlayer.getServer()).getCommands().performPrefixedCommand(
+                serverPlayer.getServer().createCommandSourceStack()
+                        .withEntity(serverPlayer)
+                        .withSuppressedOutput(),
+                command
+        );
     }
 
     public static class Builder extends SkillBuilder<FangCounterSkill> {
@@ -103,29 +162,7 @@ public class FangCounterSkill extends Skill {
         public void onInitiate(SkillContainer container) {
             PlayerEventListener listener = container.getExecutor().getEventListener();
 
-            listener.addEventListener(EventType.TAKE_DAMAGE_EVENT_ATTACK, EVENT_UUID, (event) -> {
-                        if (event.getResult() != AttackResult.ResultType.BLOCKED) return;
-
-                        ServerPlayer serverPlayer = event.getPlayerPatch().getOriginal();
-                        if (serverPlayer == null) return;
-                        //Graheh
-
-                String command = "";
-                if (!serverPlayer.level().isClientSide() && event.isParried()) {
-                    command = "/photon fx photon:breakclash4 entity @s -0.95 0.35 0.6 0 0 0 0.95 0.95 0.95 0 true true none";
-
-                }
-                else{
-                    command = "/photon fx photon:block entity @s 0 0.25 0 0 0 0 1 1 1 0 true true xrot";
-                }
-
-                Objects.requireNonNull(serverPlayer.getServer()).getCommands().performPrefixedCommand(
-                        serverPlayer.getServer().createCommandSourceStack()
-                                .withEntity(serverPlayer)
-                                .withSuppressedOutput(),
-                        command
-                );
-            });
+            listener.addEventListener(EventType.TAKE_DAMAGE_EVENT_ATTACK, EVENT_UUID, FangCounterSkill::accept);
 
             listener.addEventListener(EventType.PLAYER_KILLED_EVENT, EVENT_UUID, (event) -> {
                 int cur = container.getDataManager().getDataValue(t0001SkillDataKeys.SUPER_STACKS.get());
@@ -145,7 +182,6 @@ public class FangCounterSkill extends Skill {
                 if (!container.getExecutor().isLogicalClient()) return;
 
                 int stacks = container.getDataManager().getDataValue(t0001SkillDataKeys.SUPER_STACKS.get());
-
                 Skill skill = event.getSkillContainer().getSkill();
 
                 if (skill.getCategory() != SkillCategories.BASIC_ATTACK){
